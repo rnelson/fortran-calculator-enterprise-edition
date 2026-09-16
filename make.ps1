f130dc4e573fdb74774ef26367ea68538b2a9172
Ross Nelson <ross.nelson@gmail.com>
2026-09-16 16:56:58 -0500

    Update the build script

---
+++ make.ps1
@@ -0,0 +1,84 @@
+ifx /c ./src/main.f90 /Fosrc/main.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_001.f90 /Fosrc/calc_part_001.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_002.f90 /Fosrc/calc_part_002.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_003.f90 /Fosrc/calc_part_003.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_004.f90 /Fosrc/calc_part_004.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_005.f90 /Fosrc/calc_part_005.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_006.f90 /Fosrc/calc_part_006.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_007.f90 /Fosrc/calc_part_007.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_008.f90 /Fosrc/calc_part_008.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_009.f90 /Fosrc/calc_part_009.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_010.f90 /Fosrc/calc_part_010.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_011.f90 /Fosrc/calc_part_011.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_012.f90 /Fosrc/calc_part_012.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_013.f90 /Fosrc/calc_part_013.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_014.f90 /Fosrc/calc_part_014.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_015.f90 /Fosrc/calc_part_015.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_016.f90 /Fosrc/calc_part_016.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_017.f90 /Fosrc/calc_part_017.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_018.f90 /Fosrc/calc_part_018.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_019.f90 /Fosrc/calc_part_019.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_020.f90 /Fosrc/calc_part_020.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_021.f90 /Fosrc/calc_part_021.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_022.f90 /Fosrc/calc_part_022.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_023.f90 /Fosrc/calc_part_023.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_024.f90 /Fosrc/calc_part_024.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_025.f90 /Fosrc/calc_part_025.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_026.f90 /Fosrc/calc_part_026.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_027.f90 /Fosrc/calc_part_027.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_028.f90 /Fosrc/calc_part_028.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_029.f90 /Fosrc/calc_part_029.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_030.f90 /Fosrc/calc_part_030.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_031.f90 /Fosrc/calc_part_031.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_032.f90 /Fosrc/calc_part_032.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_033.f90 /Fosrc/calc_part_033.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_034.f90 /Fosrc/calc_part_034.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_035.f90 /Fosrc/calc_part_035.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_036.f90 /Fosrc/calc_part_036.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_037.f90 /Fosrc/calc_part_037.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_038.f90 /Fosrc/calc_part_038.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_039.f90 /Fosrc/calc_part_039.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_040.f90 /Fosrc/calc_part_040.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_041.f90 /Fosrc/calc_part_041.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_042.f90 /Fosrc/calc_part_042.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_043.f90 /Fosrc/calc_part_043.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_044.f90 /Fosrc/calc_part_044.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_045.f90 /Fosrc/calc_part_045.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_046.f90 /Fosrc/calc_part_046.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_047.f90 /Fosrc/calc_part_047.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_048.f90 /Fosrc/calc_part_048.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_049.f90 /Fosrc/calc_part_049.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_050.f90 /Fosrc/calc_part_050.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_051.f90 /Fosrc/calc_part_051.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_052.f90 /Fosrc/calc_part_052.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_053.f90 /Fosrc/calc_part_053.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_054.f90 /Fosrc/calc_part_054.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_055.f90 /Fosrc/calc_part_055.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_056.f90 /Fosrc/calc_part_056.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_057.f90 /Fosrc/calc_part_057.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_058.f90 /Fosrc/calc_part_058.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_059.f90 /Fosrc/calc_part_059.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_060.f90 /Fosrc/calc_part_060.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_061.f90 /Fosrc/calc_part_061.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_062.f90 /Fosrc/calc_part_062.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_063.f90 /Fosrc/calc_part_063.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_064.f90 /Fosrc/calc_part_064.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_065.f90 /Fosrc/calc_part_065.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_066.f90 /Fosrc/calc_part_066.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_067.f90 /Fosrc/calc_part_067.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_068.f90 /Fosrc/calc_part_068.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_069.f90 /Fosrc/calc_part_069.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_070.f90 /Fosrc/calc_part_070.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_071.f90 /Fosrc/calc_part_071.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_072.f90 /Fosrc/calc_part_072.obj /Zi /debug:all /Od
+ifx /c ./src/calc_part_073.f90 /Fosrc/calc_part_073.obj /Zi /debug:all /Od
+
+if (!(Test-Path bin/win64)) {
+	if (!(Test-Path bin)) {
+		New-Item -Type Directory bin
+	}
+
+	New-Item -Type Directory bin/win64
+}
+
+ifx ./src/*.obj /exe:bin/win64/calc.exe /Zi /debug:all /Od
